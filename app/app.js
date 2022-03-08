293eb20ecd25320233fa644703dd236bf53cbb87
Ryugi <rsuguro@gmail.com>
2022-03-08 13:15:32 +0900

    Created a custom directive

--- app/app.js
+++ app/app.js
@@ -5,18 +5,35 @@ myApp.config(['$routeProvider', function($routeProvider){
 
   $routeProvider
     .when('/home', {
-      templateUrl: 'views/home.html'
+      templateUrl: 'views/home.html',
+      controller: 'NinjaController'
     })
     .when('/directory', {
       templateUrl: 'views/directory.html',
-      controller: 'testController'
+      controller: 'NinjaController'
     }).otherwise({
       redirectTo: '/home'
     });
 
 }]);
 
-myApp.controller('testController', ['$scope', '$http', function($scope, $http){
+myApp.directive('randomNinja', [function(){
+
+  return {
+    restrict: 'E',
+    scope: {
+      ninjas: '=',
+      title: '='
+    },
+    templateUrl: 'views/random.html',
+    controller: function($scope){
+      $scope.random = Math.floor(Math.random() * 4)
+    }
+  };
+
+}]);
+
+myApp.controller('NinjaController', ['$scope', '$http', function($scope, $http){
 
   $scope.removeNinja = function(ninja){
     var removedNinja = $scope.ninjas.indexOf(ninja);
